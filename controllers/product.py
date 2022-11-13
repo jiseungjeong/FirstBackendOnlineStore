de53adaa0e2d69790ab8773ce4eef924d4392d3d
jiseungjeong <wjdwltmd1151@gmail.com>
2022-11-13 20:05:28 +0900

    upload product image

--- controllers/product.py
+++ controllers/product.py
@@ -1,8 +1,11 @@
 # about product
 from flask import request
+from werkzeug.utils import secure_filename
 from .blueprint import product  # use blueprint.py
-from models.mongodb import conn_mongodb
+# from models.mongodb import conn_mongodb
 from models.product import Product
+from datetime import datetime
+import os
 
 # product resgistration API code
 # @product.route('/regist', methods=['POST'])
@@ -17,10 +20,26 @@ def regist():
     # description = form_data["description"]
 
     # save image info.
+    thumbnail_img = request.files.get('thumbnail_img')
+    detail_img = request.files.get('detail_img')
+    thumbnail_img_url = _upload_file(thumbnail_img)
+    detail_img_url = _upload_file(detail_img)
+
     # saving works
-    Product.insert_one(form_data)
+    Product.insert_one(form_data, thumbnail_img_url, detail_img_url)
     return "This is product registration API"
 
 # @product.route('/test', methods=['GET'])
 # def test():
 #     return "This is product test API"
+
+
+def _upload_file(img_file):
+    timestamp = str(datetime.now().timestamp())
+    filename = timestamp+'_'+secure_filename(img_file.filename)
+    image_path = f'./static/uploads'
+    os.makedirs(image_path, exist_ok=True)
+    img = os.path.join(image_path, filename)
+    img_file.save(img)
+
+    return f'/static/uploads/'+filename
--- models/product.py
+++ models/product.py
@@ -1,9 +1,18 @@
 from .mongodb import conn_mongodb
+from datetime import datetime
 
 
 class Product():
     @staticmethod
-    def insert_one(product):
+    def insert_one(product, thumbnail_img_url, detail_img_url):
         db = conn_mongodb()
-        db.products.insert_one(
-            {'name': product['name'], 'price': product['price'], 'description': product['description']})
+        db.products.insert_one({
+            'name': product['name'],
+            'price': product['price'],
+            'description': product['description'],
+            'thumbnail_img': thumbnail_img_url,
+            'detail_img': detail_img_url,
+            'user': 'admin',
+            'created_at': int(datetime.now().timestamp()),
+            'updated_at':int(datetime.now().timestamp())
+        })
